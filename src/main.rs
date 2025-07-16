83e1d4e765ac1e7038b6ba5d29bee81debd6c657
Sebastian Rodriguez Colina <srcolinas@gmail.com>
2025-07-16 09:16:48 -0500

    save progress

--- src/main.rs
+++ src/main.rs
@@ -33,12 +33,14 @@ fn main() -> eframe::Result {
 struct Simulator {
     gravity: f64,
     objects: Vec<PhysicsObject>,
+    delta_time: u64,
 }
 
 impl Default for Simulator {
     fn default() -> Self {
         Self {
             gravity: 6.67430e-11, // Gravitational constant in m^3 kg^-1 s^-2
+            delta_time: 16,       // 60 FPS ~ 16 ms per frame
             objects: [
                 PhysicsObject {
                     mass: 5.972e24, // Mass of Earth in kg
@@ -52,7 +54,8 @@ impl Default for Simulator {
                     velocity: Vector2d { x: 0.0, y: 0.0 },
                     position: Vector2d { x: 700.0, y: 480.0 }, // Slightly offset from Earth
                 },
-            ].to_vec(),
+            ]
+            .to_vec(),
         }
     }
 }
@@ -68,8 +71,27 @@ impl eframe::App for Simulator {
                 let painter = ui.painter();
                 let radius = 20.0;
 
-                for object in &self.objects {
-                    // Draw each object as a circle
+                for object in &mut self.objects {
+                    let mut total_force = Vector2d { x: 0.0, y: 0.0 };
+                    for object2 in self.objects.iter().cloned() {
+                        if object as *const _ != &object2 as *const _ {
+                            let dx = object2.position.x - object.position.x;
+                            let dy = object2.position.y - object.position.y;
+                            let distance = (dx * dx + dy * dy).sqrt().max(1.0); // Avoid division by zero
+                            let force =
+                                (self.gravity * object.mass * object2.mass) / distance.powi(2);
+                            let direction_x = dx / distance;
+                            let direction_y = dy / distance;
+                            total_force.x += direction_x * force;
+                            total_force.y += direction_y * force;
+                        }
+                    }
+                    object.acceleration.x = total_force.x / object.mass;
+                    object.acceleration.y = total_force.y / object.mass;
+                    object.velocity.x += object.acceleration.x * (self.delta_time as f64);
+                    object.velocity.y += object.acceleration.y * (self.delta_time as f64);
+                    object.position.x += object.velocity.x * (self.delta_time as f64);
+                    object.position.y += object.velocity.y * (self.delta_time as f64);
                     painter.circle(
                         egui::Pos2::new(object.position.x as f32, object.position.y as f32),
                         radius,
